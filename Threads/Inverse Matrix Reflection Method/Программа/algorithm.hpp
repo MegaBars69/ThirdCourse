ea602877ab29be5359d68b58d6106ae93ecff6f5
Me <sikerin_danila@mail.ru>
2024-11-21 12:01:02 +0200

    pzdc

---
+++ Threads/Inverse Matrix Reflection Method/Программа/algorithm.hpp
@@ -0,0 +1,148 @@
+#ifndef HEADER1  
+#define HEADER1
+#include <iostream>
+#include <sys/sysinfo.h>
+#include "initialize_matrix.hpp"
+
+template <typename T>
+void reduce_sum(int p,T* a = nullptr, int n = 0)
+{
+    static pthread_mutex_t my_mutex = PTHREAD_MUTEX_INITIALIZER;
+    static pthread_cond_t c_in = PTHREAD_COND_INITIALIZER;
+    static pthread_cond_t c_out = PTHREAD_COND_INITIALIZER;
+    static int t_in = 0;
+    static int t_out = 0;
+    static T* r = nullptr;
+    int i;
+    if (p <= 1)
+    {
+        return;
+    }
+
+    pthread_mutex_lock(&my_mutex);
+
+    if (r==nullptr)
+    {
+        r = a;
+    }
+    else
+    {
+        for (i = 0; i < n; i++)
+        {
+            r[i] += a[i];
+        }
+    }
+
+    t_in++;
+
+    if (t_in >= p)
+    {
+        t_out = 0;
+        pthread_cond_broadcast(&c_in);
+    }
+    else
+    {
+        while (t_in < p)
+        {
+            pthread_cond_wait(&c_in, &my_mutex);
+        }
+    }
+
+    if(r != a)
+    {
+        for (i = 0; i < n; i++)
+        {
+            a[i] = r[i];
+        }
+    }
+    t_out++;
+    if (t_out >= p)
+    {
+        t_in = 0;
+        r = nullptr;
+        pthread_cond_broadcast(&c_out);
+    }
+    else
+    {
+        while (t_out < p)
+        {
+            pthread_cond_wait(&c_out, &my_mutex);
+        }
+        
+    }
+    pthread_mutex_unlock(&my_mutex);
+}
+
+void PrintMatrix(double* A,  int n, int m, int r, int p, bool exp_format = false, bool okruglenie = false);
+
+enum class io_status
+{
+    working,
+    not_working,
+    succes
+};
+
+class Args{
+    public:
+        int p = 0;
+        int thr_n = 0;
+        pthread_t tid = 0;
+        double cpu_time = 0;
+        double cpu_time_of_all_threads = 0;
+        double astr_time = 0;
+        io_status status = io_status::not_working;
+        double res = 0;
+
+        double* A = nullptr;
+        double* B = nullptr; 
+        double* U = nullptr;
+        double* ProductResult = nullptr;
+
+        std::string name = nullptr;
+
+        int n = 0; //Matrix dim
+        int m = 0; //Block dim
+        int M = 0; //Amount of block strings
+        int r = 0; //Amount of printing elements
+        int s = 0;
+        
+
+        void PrintAll() const
+        {
+            printf("Number of thread: %d \n", thr_n);
+            printf("p: %d \n", p);
+            printf("n: %d \n", n);
+            printf("M: %d \n", M);
+            printf("r: %d \n", r);
+            printf("s: %d \n", s);
+
+            PrintMatrix(A, n, m, r, p);
+            PrintMatrix(B, n, m, r, p);
+        }
+        
+};
+double get_cpu_time();
+double get_fun_time();
+void* thread_func(void *arg);
+void ApplyVector(double* X, double* A, int row_num, int col_num, int k, bool inside = true);
+int InverseTriungleBlock(double* A, double* B, int n, double norm);
+void ReplaceWith(double*A, double*B, int row_size, int col_size);
+void MatrixMinusEqual(double* A, double* B, int row_size, int col_size);
+void MatrixPlusEqual(double* A, double* B, int row_size, int col_size);
+void DifferenceOfBlockMatrix(double* A, double* B, int n, int m);
+void MinusEqualBlockMulEqualBlockMul(double* Main, double *a, double* b, int n1, int m12, int n2);
+
+int Triungulize(double* A, double* U, int row_num, int col_num, double norm);
+int InverseMatrix(double* A, double* B, double* U, double* ProductResult, double* ZeroMatrix, double norm, int n, int m, int S);
+void ApplyMatrix(double* U, double* A, int row_num, int col_num, int amount_of_vectors);
+void ZeroOut(double* Diag, double* Down, double* U, int m, int row_size, double norm);
+void ApplyMatrixToPair(double* U, double* Up, double* Down, int col_size, int row_size, int amount_of_vectors, bool down_is_zero = false);
+
+double Norm(double* A, double* results, int n, int m);
+void AddToMatrix(double* A, double* B, int n, int m);
+void BlockMul(double *a, double* b, double* c, int n1, int m12, int n2);
+void BlockMulOptimized(double *a, double* b, double* c, int n1, int m12, int n2);
+double CalcError(double * A, double * InversedA, int n, int m);
+double Discrepancy(double * A, double * InversedA, double* Column, double* ProductResult, double* Sum, int n, int m);
+
+#endif 
---
+++ Threads/Inverse Matrix Reflection Method/Программа/main.cpp
@@ -0,0 +1,585 @@
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <sstream>
+#include <ctime>
+#include <cmath>
+#include "initialize_matrix.hpp"
+#include "algorithm.hpp"
+
+using namespace std;
+/*
+int main(int argc, char* argv[]) {
+    
+    if (argc < 5) {
+        cerr << "Usage: " << argv[0] << " n m r s [filename]" << endl;
+        return 1;
+    }
+    
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    int r = atoi(argv[3]);
+    int s = atoi(argv[4]);
+    int result = 0;
+    //int end_of_inverse = 0;
+    double t1 = 0, t2 = 0, r1 = -1, r2 = -1;
+
+    double* A = new double[n * n];
+    double* B = new double[n * n];
+    double* U = new double[(m+1)*(m+1)];
+    double* ProductResult = new double[m*m];
+
+    //Зануляем вспомогательную матрицу
+    for (int i = 0; i < (m+1)*(m+1); i++)
+    {
+        U[i] = 0;
+    }
+    for (int i = 0; i < m*m; i++)
+    {
+        ProductResult[i] = 0;
+    }
+    
+    
+    if(m <= 0 || s < 0 || s > 4 || n <= 0)
+    {
+        cerr << "Usage: " << argv[0] << " n m r s [filename]" << endl;
+        return 1;
+    }
+
+    //Инициализируем матрицы А и В
+    if (s == 0) {
+        string filename = argv[5];
+        result = ReadMatrixFromFile(filename, A, n, m);
+    } else {
+        FormulaMatrixInitialization(A, n, m, s);
+    }
+    BuildE(B, n, m);
+
+    //Проверка входного файла
+    if (result == -1)
+    {
+        printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", argv[0], 24, r1, r2, t1, t2, s, n, m);
+        delete[] A;
+        delete[] B;
+        delete[] U;
+        delete[] ProductResult;
+        return 0;
+    }
+    PrintMatrix(A, n, m, r);
+    PrintMatrix(B, n, m, r);
+    double* X = new double[5*4];
+    double* Z = new double[4*6];
+    X[0] = 1; X[1] = 11; X[2] = 111 ; X[3] = 1111;
+    X[4] = 2; X[5] = 22; X[6] = 222; X[7] = 2222;
+    X[8] = 3; X[9] = 33; X[10] = 333; X[11] = 3333;
+    X[12] = 4; X[13] = 44; X[14] = 444; X[15] = 4444;
+    X[16] = 5; X[17] = 55; X[18] = 555; X[19] = 5555;
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout<<X[i*4+j]<<"  ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    Z[0] = 9; Z[1] = 8; Z[2] = 7; Z[3] = 6; Z[4] = 5; Z[5] = 4;
+    Z[6] = 3; Z[7] = 2; Z[8] = 1; Z[9] = 0; Z[10] = -1; Z[11] = -2;
+    Z[12] = -3; Z[13] = -4; Z[14] = -5; Z[15] = -6; Z[16] = -7; Z[17] = -8;
+    Z[18] = -9; Z[19] = -10; Z[20] = -11; Z[21] = -12; Z[22] = -13; Z[23] = -14; 
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            cout<<Z[i*6+j]<<"  ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+
+    BlockMulOptimezed(X, Z, ProductResult, 5, 4, 6);
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            cout<<ProductResult[i*6+j]<<"  ";
+        }
+        cout<<endl;
+    }
+
+    
+    cout<<endl;
+    BlockMul(X, Z, ProductResult, 5,4,6);
+    
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            cout<<ProductResult[i*6+j]<<"  ";
+        }
+        cout<<endl;
+    }
+
+    
+    //Вычисление обратной матрицы
+    clock_t start1 = clock();
+
+    //end_of_inverse = InverseMatrix(A, B, U, ProductResult, n, m);
+
+    clock_t end1 = clock();
+    
+    t1 = static_cast<double>(end1 - start1) / CLOCKS_PER_SEC;
+    //Инициализируем матрицу для подсчета невязки
+    
+    if (s == 0) {
+        string filename = argv[5];
+        result = ReadMatrixFromFile(filename, A, n, m);
+    } else {
+        FormulaMatrixInitialization(A, n, m, s);
+    }
+
+    if (end_of_inverse != 0)
+    {    
+        PrintMatrix(A, n, m, r, true, false);
+        printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", argv[0], 24, r1, r2, t1, t2, s, n, m);
+        delete[] A;
+        delete[] B;
+        delete[] U;
+        delete[] ProductResult;
+        return 0;
+    }
+
+    //Печать ответа
+    PrintMatrix(A, n, m, r, true, false);
+    PrintMatrix(B, n, m, r, true, false);
+    
+    //Подсчет невязки
+    clock_t start2 = clock();
+
+    //r1 = Discrepancy(A, B, n, m);
+    //r2 = Discrepancy(B, A, n, m);
+     
+    clock_t end2 = clock();
+
+    t2 = static_cast<double>(end2 - start2) / CLOCKS_PER_SEC;
+    
+    printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", argv[0], 24, r1, r2, t1, t2, s, n, m);
+
+    delete[] A;
+    delete[] B;
+    delete[] U;
+    delete[] ProductResult;
+    return 0;
+}
+*/
+
+int main(int argc, char* argv[]) {
+    /*
+    if (argc < 5) {
+        cerr << "Usage: " << argv[0] << " n m r s [filename]" << endl;
+        return 1;
+    }
+    
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    int r = atoi(argv[3]);
+    int s = atoi(argv[4]);
+    int result = 0;
+    int end_of_inverse = 0;
+    double t1 = 0, t2 = 0, r1 = -1, r2 = -1;
+
+    double* A = new double[n * n];
+    double* B = new double[n * n];
+    double* U = new double[(m+1)*(m+1)];
+    double* ProductResult = new double[m*m];
+    double* results = new double[m];
+    double* Sum = new double[m*m];
+    double* Column = new double[m];
+
+    //Зануляем вспомогательную матрицу
+    for (int i = 0; i < (m+1)*(m+1); i++)
+    {
+        U[i] = 0;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            Sum[i*m + j] = 0;
+            ProductResult[i*m +j] = 0;
+        }  
+    } 
+    for (int i = 0; i < m; i++)
+    {
+        results[i] = 0;
+    }
+    
+    if(m <= 0 || s < 0 || s > 4 || n <= 0)
+    {
+        cerr << "Usage: " << argv[0] << " n m r s [filename]" << endl;
+        return 1;
+    }
+
+    //Инициализируем матрицы А и В
+    if (s == 0) {
+        if (argc >5)
+        {
+            string filename = argv[5];
+            result = ReadMatrixFromFile(filename, A, n, m);
+        }
+        else
+        {
+            result = -1;
+        }
+        
+    } else {
+        FormulaMatrixInitialization(A, n, m, s);
+    }
+    BuildE(B, n, m);
+
+
+    //Проверка входного файла
+    if (result == -1)
+    {
+        printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", argv[0], 24, r1, r2, t1, t2, s, n, m);
+        delete[] A;
+        delete[] B;
+        delete[] U;
+        delete[] ProductResult;
+        delete[] results;
+        delete[] Sum;
+        delete[] Column;
+        return 0;
+    }
+        
+    double norm = Norm(A, results, n, m);
+
+    //Вычисление обратной матрицы
+    clock_t start1 = clock();
+
+    end_of_inverse = InverseMatrix(A, B, U, ProductResult, Sum, norm, n, m, s);
+
+    clock_t end1 = clock();
+    
+    t1 = static_cast<double>(end1 - start1) / CLOCKS_PER_SEC;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+    //Инициализируем матрицу для подсчета невязки
+    
+    if (s == 0) {
+        string filename = argv[5];
+        result = ReadMatrixFromFile(filename, A, n, m);
+    } else {
+        FormulaMatrixInitialization(A, n, m, s);
+    }
+     
+    if (end_of_inverse != 0)
+    {    
+        PrintMatrix(A, n, m, r, true, false);
+        printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", argv[0], 24, r1, r2, t1, t2, s, n, m);
+        delete[] A;
+        delete[] B;
+        delete[] U;
+        delete[] ProductResult;
+        delete[] results;
+        delete[] Sum;
+        delete[] Column;
+        return 0;
+    }
+
+    //Печать ответа
+    PrintMatrix(A, n, m, r, true, false);
+    PrintMatrix(B, n, m, r, true, false);
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            Sum[i*m + j] = 0;
+            ProductResult[i*m +j] = 0;
+        }  
+    }  
+     for (int j = 0; j < m; j++)
+    {
+        Column[j] = 0;
+    }     
+
+
+    //Подсчет невязки
+    clock_t start2 = clock();
+    if (n <= 11000)
+    {
+        r1 = Discrepancy(A, B, Column, ProductResult, Sum, n, m);
+        r2 = Discrepancy(B, A, Column, ProductResult, Sum, n, m);
+    }
+    else
+    {
+        r1 = 0;
+        r2 = 0;
+    }
+    clock_t end2 = clock();
+
+    t2 = static_cast<double>(end2 - start2) / CLOCKS_PER_SEC;
+    
+    printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", argv[0], 24, r1, r2, t1, t2, s, n, m);
+    */
+
+    Args* a;
+    double* A, *B, **U, **ProductResult;
+    double el;
+    int i = 0, k;
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    int r = atoi(argv[3]);
+    int p = atoi(argv[4]);
+    int s = atoi(argv[5]);
+    std::string filename = argv[6];
+
+    if(argc == 1)
+    {
+        printf("Usage ./a.out n p r s filename\n");
+        return 1;
+    }
+
+
+    if (p <= 0)
+    {
+        printf(" p > 0 \n");
+        return 4;
+    }
+    
+    if (p > 0)
+    {
+        a = new Args[p];
+        if (n > 0)
+        {
+            A = new double[n*n];
+            B = new double[n*n];
+            U = new double*[p];
+            ProductResult = new double*[p];
+            for (int i = 0; i < p; i++)
+            {
+                U[i] = new double[(m+1)*(m+1)];
+                ProductResult[i] = new double[m*m];
+            }
+        }
+        
+    }
+    else
+    {
+        printf(" p >=0");
+        return 4;
+    }        
+
+    //Обработка массива. Распределение памяти.
+    for (k = 0; k < p; k++)
+    {
+        a[k].thr_n = k;
+        a[k].p = p;
+        a[k].n = n;
+        
+        a[k].name = filename;
+
+        a[k].A = A;
+        a[k].B = B;
+        a[k].U = U[k];
+        a[k].ProductResult = ProductResult[k];
+        
+        a[k].s = s;
+        a[k].r = r;
+    }
+
+    //Запуск потоков.
+    for (k = 1; k < p; k++)
+    {
+        if (pthread_create(&a[k].tid, nullptr, thread_func, a+k))
+        {
+            std::cerr << "Error creating thread " << k <<std::endl;
+            
+            delete[] A;
+            delete[] B;
+            delete[] U;
+            delete[] a;
+            delete[] ProductResult;
+            return 1;
+        }
+    }
+    a[0].tid = pthread_self();
+
+    thread_func(a+0);
+
+    //Прибиваем потоки молотком.
+    for (k = 1; k < p; k++)
+    {
+        pthread_join(a[k].tid, nullptr);
+    }
+    
+    /*for (k = 0; k < p; k++)
+    {
+        a[k].PrintAll();
+    }*/
+    
+        
+    
+    delete[] A;
+    delete[] B;
+    delete[] a;
+    delete[] U;
+    delete[] ProductResult;
+    return 0;
+}
+
+/*
+int main()
+{
+    int n = 100;
+
+    int m = 3;
+    int r = n;
+    int s = 2;
+    double t1 = 0, t2 = 0, r1 = -1, r2 = -1;
+    double* A = new double[n * n];
+    double* B = new double[n * n];
+    double* AA = new double[n * n];
+    double* BB = new double[n * n];
+    double* product_result = new double[m * m];
+    double* C = new double[n * n];
+    double* D = new double[n * n];
+
+
+    double* U = new double[(m+1)*(m+1)];
+    int result = 0;
+
+    
+    FormulaMatrixInitialization(A, n, m, s);
+    FormulaMatrixInitialization(AA, n, 1, s);
+    //FormulaMatrixInitialization(B, n, 1, 1);
+    //double B[6] = {1, 2, 3, 4, 5, 6};
+    //result = ReadMatrixFromFile("input.txt", A, n, m);
+    //result = ReadMatrixFromFile("input.txt", AA, n, 1);
+    if (result == -1)
+    {
+        printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", "a.out", 24, r1, r2, t1, t2, s, n, m);
+        delete[] A;
+        delete[] B;
+        delete[] U;
+        delete[] BB;
+        delete[] AA;
+        delete[] product_result;
+        delete[] C;
+        return 0;
+    }
+    
+    //ReadMatrixFromFile("inverse.txt", B, n, m);
+
+    //BuildE(A, n, m);
+
+    //BuildE(D, n, m);
+    BlockMul(AA, B,C, n,n,2);
+    PrintMatrix(AA, n, 1, r, false);
+    PrintMatrix(B, n, 1, r, false);
+    PrintMatrix(C, n, 1, r, false);
+
+    
+    
+    cout<<"Initialization"<<endl;
+    BuildE(B, n, m);
+    BuildE(BB, n, m);
+    PrintMatrix(A,n, m, r);
+    PrintMatrix(B, n, m, r);
+    //PrintMatrix(AA,n, 1, r);
+    PrintMatrix(BB,n, 1, r);
+    
+    
+    PrintMatrix(C, n, m, r);
+    PrintMatrix(D, n, m, r);
+
+    
+    cout<<"Triungulize"<<endl;
+    if(Triungulize(A, U, n, n) == 0)
+    {
+        PrintMatrix(A, n, m, r);
+        PrintMatrix(U, m, m, r);
+        ApplyMatrix(U, B, n, n);
+        PrintMatrix(B, n, m, r, false);
+        
+        cout<<"Second Step"<<endl;
+        ZeroOut(A, C, U, m, m);
+        PrintMatrix(A, n, m, r);
+        PrintMatrix(C, n, m, r);
+        PrintMatrix(U, m+1, m+1, r+1);
+        ApplyMatrixToPair(U, B, D, n, n);
+        PrintMatrix(B, n, m, r);
+        PrintMatrix(D, n, m, r);
+    }
+    clock_t start1 = clock();
+    
+    if(InverseMatrix(A, B, U, product_result, n, m) == 0)
+    {
+        PrintMatrix(A,n, m, r);
+        PrintMatrix(B, n, m, r);
+    }
+    
+    U = new double[(n+1)*(n+1)];
+    cout<<"ANSWER";
+    Triungulize(AA, U, n, n);
+    ApplyMatrix(U, BB, n, n, n);
+    InverseTriungleBlock(AA, BB, n, false);
+
+    PrintMatrix(AA, n, 1, r);
+    PrintMatrix(A, n, m, r);
+    //PrintMatrix(BB, n, 1, r);
+    PrintMatrix(B, n, m, r);
+    
+    clock_t start1 = clock();
+    if(InverseMatrix(A, B, U, product_result, n, m) == 0)
+    {
+        PrintMatrix(A,n, m, r);
+        PrintMatrix(B, n, m, r);
+    }
+    
+    
+    Triungulize(AA, U, n, n);
+    ApplyMatrix(U, BB, n, n, n);
+
+    
+    ReplaceWith(BB, AA, n, n);
+    PrintMatrix(AA, n, 1, r);
+    PrintMatrix(BB, n, 1, r);
+    //PrintMatrix(U, n, 1, n);
+   
+    //result = ReadMatrixFromFile("input.txt", AA, n, 1);    
+    clock_t end1 = clock();
+    
+    t1 = static_cast<double>(end1 - start1) / CLOCKS_PER_SEC;
+
+    clock_t start2 = clock();
+
+    //r1 = CalcError(A, B, n, m);
+    FormulaMatrixInitialization(A, n, m, s);
+
+    r1 = Discrepancy(A, B, n, m);
+    //FormulaMatrixInitialization(A, n, m, s);
+
+    //r2 = CalcError(B, A, n, m);
+    r2 = Discrepancy(B, A, n, m);
+     
+    clock_t end2 = clock();
+    
+    t2 = static_cast<double>(end2 - start2) / CLOCKS_PER_SEC;
+    
+    cout<<endl;
+    printf ("%s : Task = %d Res1 = %e Res2 = %e T1 = %.2f T2 = %.2f S = %d N = %d M = %d\n", "a.out", 24, r1, r2, t1, t2, s, n, m);
+
+    delete[] A;
+    delete[] B;
+    delete[] U;
+    delete[] BB;
+    delete[] AA;
+    delete[] product_result;
+    delete[] C;
+
+
+    return 0;
+}*/
