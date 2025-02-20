80f401a43e8732448eee781612280e2afd158673
SiDan <73333249+MegaBars69@users.noreply.github.com>
2025-02-20 14:35:55 +0300

    Delete Block Inverse Reflection Method directory

--- Block Inverse Reflection Method/Программа/initialize_matrix.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef HEADER2  
-#define HEADER2
-#include <iostream>
-
-int ReadMatrixFromFile(const std::string& filename, double* A,  int n, int m);
-void BuildE(double* B, int n, int m);
-void FormulaMatrixInitialization(double* A, int n, int m, int s);
-
-#endif 
